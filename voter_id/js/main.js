ae426d7a2c22a6310c7adb0a0863717ef28e4881
jaya-sri-j <jayasrijagan28@gmail.com>
2022-12-26 10:52:05 +0530

    adding voter id program.

---
+++ voter_id/js/main.js
@@ -0,0 +1,18 @@
+var age=parseInt(prompt("enter the age"));
+var voteid="ok";
+if(age>=18)
+{
+if(voteid === "ok")
+{
+	console.log(" you are eligible");
+}
+else
+{
+	console.log("you have not eligible");
+}
+}
+
+else
+{
+	console.log("you are young");
+}
